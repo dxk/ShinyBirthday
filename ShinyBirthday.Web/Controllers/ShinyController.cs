990610165e69300968140a9cc1b1dd759f3a2ddd
dxk <dxk@you-may.cn>
2011-11-24 16:16:46 +0800

    Signed-off-by: dxk <dxk@you-may.cn>

---
+++ ShinyBirthday.Web/Controllers/ShinyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShinyBirthday.Web.Controllers
+{
+    public class ShinyController : Controller
+    {
+        //
+        // GET: /Shiny/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
--- ShinyBirthday.Web/Global.asax.cs
+++ ShinyBirthday.Web/Global.asax.cs
@@ -24,7 +24,7 @@ namespace ShinyBirthday.Web
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Shiny", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
